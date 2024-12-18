d1e641ed1caf6e78c13b9786cf2e070c7f967b33
Kirana Trupthi Shree <kiranats2006@gmail.com>
2024-12-18 10:35:25 +0530

    milestone8 completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import LoginPage from "./components/auth/login";
-import SignUpPage from "./components/auth/signup";
+// import LoginPage from "./components/auth/login";
+// import SignUpPage from "./components/auth/signup";
 import { Routes, Route } from 'react-router-dom';
-
+import HomePage from './Pages/HomePage';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
 function App() {
   return (
-    <Routes>
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/login" element={<LoginPage/>}/>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Card from "../components/ProdCard/Card";
+
+function HomePage() {
+  const [data, setData] = useState(
+    new Array(20).fill(null).map(() => ({ title: "Product Title" }))
+  );
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-pink-500 via-green-500 to-pink-500 font-serif">
+      <div className="bg-white p-8 shadow-lg rounded-lg max-w-6xl w-full z-10">
+        <h2 className="text-3xl font-bold mb-8 text-center text-purple-500">
+          Our Products
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {data.map((ele, index) => (
+            <div
+              key={index}
+              className="flex justify-center items-center border border-gray-300 rounded-lg p-4 shadow-md hover:shadow-lg"
+            >
+              <Card title={ele.title} Index={index} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
