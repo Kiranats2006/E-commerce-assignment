0c6a7340693e32f7039a382f58b638140a03e9a2
Kirana Trupthi Shree <kiranats2006@gmail.com>
2025-02-04 10:03:28 +0530

    Milestone 26

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken  = require('../middleware/jwt-verify');
+const {
+    GetUserOrdersController,
+    CreateOrderController,
+  } = require('../controllers/order.controller');
 // POST - /confirm-order {addrss, items, totalAmount}
-router.post('/confirm-order', verifyToken /* controller */);
+router.get('/user-orders-data', verifyToken, GetUserOrdersController);
+router.post('/confirm-order', verifyToken, CreateOrderController);
 module.exports = router;
